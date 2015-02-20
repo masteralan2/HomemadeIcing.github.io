60c918dce114dbdf6c850945edc912c2af488573
Alan <alanng95@hotmail.com>
2015-02-20 16:04:14 +1100

    fixed fb login

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -8,6 +8,30 @@ FB.init({
 FB.Event.subscribe('auth.authResponseChange', onAuthResponseChange);
 FB.Event.subscribe('auth.statusChange', onStatusChange);
 
+// Place following code after FB.init call.
+
+function onLogin(response) {
+  if (response.status == 'connected') {
+    FB.api('/me?fields=first_name', function(data) {
+      var welcomeBlock = document.getElementById('fb-welcome');
+      welcomeBlock.innerHTML = 'Hello, ' + data.first_name + '!';
+    });
+  }
+}
+
+FB.getLoginStatus(function(response) {
+  // Check login status on load, and if the user is
+  // already logged in, go directly to the welcome message.
+  if (response.status == 'connected') {
+    onLogin(response);
+  } else {
+    // Otherwise, show Login dialog first.
+    FB.login(function(response) {
+      onLogin(response);
+    }, {scope: 'user_friends, email'});
+  }
+});
+
 Candy.MainMenu = function(game){};
 Candy.MainMenu.prototype = {
 	create: function(){
