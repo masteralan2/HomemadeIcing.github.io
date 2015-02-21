93a5a8d1a2e229e85775e559674d9601a7cd2a69
Alan <alanng95@hotmail.com>
2015-02-21 22:57:42 +1100

    test for mobile

--- src/Boot.js
+++ src/Boot.js
@@ -14,9 +14,9 @@ Candy.Boot.prototype = {
 		this.input.maxPointers = 2;
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		//if its not a desktop and isn't in landscape on mobile
-		if (!this.game.device.desktop && !this.isFFOS) {
-			this.scale.forceOrientation(true, false, 'screenRotate')
-		}
+		// if (!this.game.device.desktop && !this.isFFOS) {
+		// 	this.scale.forceOrientation(true, false, 'screenRotate')
+		// }
 		this.scale.pageAlignHorizontally = true;
 		this.scale.pageAlignVertically = true;
 		this.scale.setScreenSize(true);
@@ -26,7 +26,7 @@ Candy.Boot.prototype = {
 	//in the phaser cdn file, you can check the navigator (the device the user is using) to see 
 	//if the user is using mobile. You can be more specifc like say 'iphone', or 'android'.
 
-	isFFOS: function() {
-    return (!!"mozApps" in navigator && navigator.userAgent.search("Mobile")) != -1
-  }
+	// isFFOS: function() {
+ //    return (!!"mozApps" in navigator && navigator.userAgent.search("Mobile")) != -1
+ //  }
 };
