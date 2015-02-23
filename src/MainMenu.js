78331a6bd24a491011eb751e85ed3280aa18f1fb
Alan <alanng95@hotmail.com>
2015-02-23 13:54:13 +1100

    unminified versions to show for jack

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -1,45 +1,129 @@
-Candy.MainMenu = function(game){};
+Candy.MainMenu = function(game) {
+  blackmoonButton = null;
+  enclaveButton = null;
+  audioStatus = false;
+  audioOffset = 0;
+  soundClick = null;
+  soundMusic = null
+};
 Candy.MainMenu.prototype = {
-	create: function(){
-		// display images
-		this.add.sprite(0, 0, 'background');
-		this.add.sprite(-170, Candy.GAME_HEIGHT - 514 + 20, 'alan-cover');
-		this.add.sprite(Candy.GAME_WIDTH - 395 - 5, 140, 'title');
-		// add the button that will start the game
-		this.buttonStart = this.add.button(Candy.GAME_WIDTH, Candy.GAME_HEIGHT-143-10, 'button-start', this.startGame, this, 1, 0, 2);
-		this.buttonAlanWebsite = this.add.button(Candy.GAME_WIDTH, Candy.GAME_HEIGHT - 143 - 130 - 130 - 3, 'button-alans-website', this.alansWebsite, this, 1, 0, 2);
-		this.buttonAlanWebsite.scale = {
-      x: 0.75,
-      y: 0.75
-    };
+  create: function() {
+    this.add.sprite(0, 0, 'background');
+    this.add.sprite(-170, Candy.GAME_HEIGHT - 514 + 20, 'monster-cover');
+    highscoreTxt = this.add.sprite(10, 140, 'text-highscore');
+    highscoreBg = this.add.sprite(5, 210, 'score-bg');
+    highscoreText = this.add.text(150, 252, "0", {
+      font: "36px ComicBook",
+      fill: "#FFCC00"
+    });
+    highscoreText.anchor.setTo(0.5, 0.5);
+    highscoreText.x = (213 + 75 - highscoreText.width) * 0.5;
+    overallTxt = this.add.sprite(10, 310, 'text-overall');
+    overallBg = this.add.sprite(5, 360, 'score-bg');
+    totalscoreText = this.add.text(150, 402, "0", {
+      font: "36px ComicBook",
+      fill: "#FFCC00"
+    });
+    totalscoreText.anchor.setTo(0.5, 0.5);
+    totalscoreText.x = (213 + 75 - totalscoreText.width) * 0.5;
+    var title = this.add.sprite(Candy.GAME_WIDTH - 395 - 5, 140, 'title');
+    this.buttonStart = this.add.button(Candy.GAME_WIDTH, Candy.GAME_HEIGHT - 143 - 10, 'button-start', this.clickStart, this, 1, 0, 2);
+    this.buttonMoreGames = this.add.button(Candy.GAME_WIDTH - 300, Candy.GAME_HEIGHT - 440, 'button-alanswebsite', this.clickAlansWebsite, this, 1, 0, 2);
+    this.buttonAchievements = this.add.button(Candy.GAME_WIDTH, Candy.GAME_HEIGHT - 143 - 130 - 20, 'button-achievements', this.clickAchievements, this, 1, 0, 2);
+
+    this.buttonAudio = this.add.button(Candy.GAME_WIDTH - 111 - 10, -96, 'button-audio', this.manageAudio, this, 1, 0, 2);
     this.buttonStart.input.useHandCursor = true;
-    this.buttonAlanWebsite.input.useHandCursor = true;
+    this.buttonMoreGames.input.useHandCursor = true;
+    this.buttonAchievements.input.useHandCursor = true;
+    this.buttonAudio.input.useHandCursor = true;
+    storageAPI.initUnset('highscore', 0);
+    var highscore = storageAPI.get('highscore');
+    highscoreText.setText(highscore);
+    storageAPI.initUnset('totalscore', 0);
+    var totalscore = storageAPI.get('totalscore');
+    totalscoreText.setText('' + totalscore);
+    this.initAudio();
     this.add.tween(this.buttonStart).to({
       x: Candy.GAME_WIDTH - 401 - 10
     }, 1000, Phaser.Easing.Exponential.Out, true, 0, false);
-    this.add.tween(this.buttonAlanWebsite).to({
-      x: Candy.GAME_WIDTH - 268 - 15
+    this.add.tween(this.buttonAchievements).to({
+      x: Candy.GAME_WIDTH - 363 - 10
+    }, 1000, Phaser.Easing.Exponential.Out, true, 100, false);
+    this.add.tween(this.buttonMoreGames).to({
+      x: Candy.GAME_WIDTH - 380
     }, 1000, Phaser.Easing.Exponential.Out, true, 200, false);
-	},
-	initAudio: function() {
-		storageAPI.initUnset('audio', false);
-	},
-
-	startGame: function() {
-		// start the Game state
-		// if (this.audioStatus) {
-		// 	this.soundClick.play()
-		// }
-		// this.audioStatus = false;
-		// this.audioOffset = 0;
-		// this.soundMusic.stop();
-		this.state.start('Game');
-	},
-
-	alansWebsite: function() {
-		// if(this.audioStatus) {
-		// 	this.soundClick.play()
-		// }
-		window.top.open('http://homemadeicing.github.io')
-	}
+    this.add.tween(this.buttonAudio).to({
+      y: 10
+    }, 1000, Phaser.Easing.Bounce.Out, true, 100, false);
+    highscoreText.alpha = 0;
+    highscoreTxt.alpha = 0;
+    highscoreBg.alpha = 0;
+    totalscoreText.alpha = 0;
+    overallTxt.alpha = 0;
+    overallBg.alpha = 0;
+    this.add.tween(highscoreText).to({
+      alpha: 1
+    }, 500, Phaser.Easing.Linear.None, true, 0, 0, false);
+    this.add.tween(highscoreTxt).to({
+      alpha: 1
+    }, 500, Phaser.Easing.Linear.None, true, 0, 0, false);
+    this.add.tween(highscoreBg).to({
+      alpha: 1
+    }, 500, Phaser.Easing.Linear.None, true, 0, 0, false);
+    this.add.tween(totalscoreText).to({
+      alpha: 1
+    }, 500, Phaser.Easing.Linear.None, true, 0, 0, false);
+    this.add.tween(overallTxt).to({
+      alpha: 1
+    }, 500, Phaser.Easing.Linear.None, true, 0, 0, false);
+    this.add.tween(overallBg).to({
+      alpha: 1
+    }, 500, Phaser.Easing.Linear.None, true, 0, 0, false)
+  },
+  initAudio: function() {
+    storageAPI.initUnset('audio', false);
+    this.audioStatus = storageAPI.get('audio');
+    this.soundClick = this.add.audio('audio-click');
+    this.soundMusic = this.add.audio('audio-music', 1, true);
+    this.soundMusic.volume = 0.5;
+    if (this.audioStatus) {
+      this.audioOffset = 0;
+      this.soundMusic.play('', 0, 1, true)
+    } else {
+      this.audioOffset = 3
+    }
+    this.buttonAudio.setFrames(this.audioOffset + 1, this.audioOffset + 0, this.audioOffset + 2)
+  },
+  manageAudio: function() {
+    this.audioStatus = !this.audioStatus;
+    storageAPI.set('audio', this.audioStatus);
+    if (this.audioStatus) {
+      this.audioOffset = 0;
+      this.soundMusic.play('', 0, 1, true);
+      this.soundClick.play()
+    } else {
+      this.audioOffset = 3;
+      this.soundMusic.stop()
+    }
+    this.buttonAudio.setFrames(this.audioOffset + 1, this.audioOffset + 0, this.audioOffset + 2)
+  },
+  clickStart: function() {
+    if (this.audioStatus) {
+      this.soundClick.play()
+    }
+    this.state.start('Game')
+  },
+  clickAchievements: function() {
+    if (this.audioStatus) {
+      this.soundClick.play();
+      this.soundMusic.stop()
+    }
+    this.state.start('Achievements')
+  },
+  clickAlansWebsite: function() {
+    if (this.audioStatus) {
+      this.soundClick.play()
+    }
+    window.top.open('http://homemadeicing.github.io')
+  }
 };
--- src/Preloader.js
+++ src/Preloader.js
@@ -1,33 +1,86 @@
-Candy.Preloader = function(game){
-	// define width and height of the game
-	this.background = null;
-	this.preloadBar = null;
-	Candy.GAME_WIDTH = 640;
-	Candy.GAME_HEIGHT = 960;
+Candy.Preloader = function(game) {
+  this.background = null;
+  this.preloadBar = null;
+  Candy.GAME_WIDTH = 640;
+  Candy.GAME_HEIGHT = 960
 };
 Candy.Preloader.prototype = {
-	preload: function(){
-		// set background color and preload image
-		this.stage.backgroundColor = 'ffffff';
-		this.add.sprite(0, 0, 'preloaderBackground');
-		this.preloadBar = this.add.sprite((Candy.GAME_WIDTH - 289) / 2, (Candy.GAME_HEIGHT - 45 + 230) / 2, 'preloaderBar');
-		this.load.setPreloadSprite(this.preloadBar);
-		// load images
-		this.load.image('background', 'img/background.png');
-		this.load.image('alan-cover', 'img/alan-cover.png');
-		this.load.image('title', 'img/title.png');
-		this.load.image('game-over', 'img/gameover.png');
-		this.load.image('score-bg', 'img/score-bg.png');
-		this.load.image('button-pause', 'img/button-pause.png');
-		this.load.image('button-restart', 'img/button-restart.png');
-		this.load.spritesheet('button-alans-website', 'img/button-alans-website.png', 358, 133);
-		// load spritesheets
-		this.load.spritesheet('candy', 'img/candy.png', 82, 98);
-		// this.load.spritesheet('monster-idle', 'img/monster-idle.png', 103, 131);
-		this.load.spritesheet('button-start', 'img/button-start.png', 401, 143);
-	},
-	create: function(){
-		// start the MainMenu state
-		this.state.start('MainMenu');
-	}
+  preload: function() {
+    this.stage.backgroundColor = '#B4D9E7';
+    this.add.sprite(0, 0, 'preloaderBackground');
+    this.preloadBar = this.add.sprite((Candy.GAME_WIDTH - 289) / 2, (Candy.GAME_HEIGHT - 45 + 230) / 2, 'preloaderBar');
+    this.load.setPreloadSprite(this.preloadBar);
+    this.load.image('background', 'img/background.png');
+    this.load.image('floor', 'img/floor.png');
+    this.load.image('monster-cover', 'img/monster-cover.png');
+    this.load.image('title', 'img/title.png');
+    this.load.image('explosion', 'img/explosion.png');
+    this.load.image('score-bg', 'img/score-bg.png');
+    this.load.image('monster-sleeps', 'img/monster-sleeps.png');
+    this.load.image('screen-overlay', 'img/screen-overlay.png');
+    this.load.image('screen-achievements', 'img/screen-achievements.png');
+    this.load.image('screen-completed', 'img/screen-completed.png');
+    this.load.image('text-paused', 'img/text-paused.png');
+    this.load.image('text-gameover', 'img/text-gameover.png');
+    this.load.image('text-highscore', 'img/text-highscore.png');
+    this.load.image('text-overall', 'img/text-overall.png');
+    this.load.image('text-newbestscore', 'img/text-newbestscore.png');
+    this.load.image('message-newcandy', 'img/message-newcandy.png');
+    this.load.image('candy-red', 'img/candy-red.png');
+    this.load.image('candy-marshmallow', 'img/candy-marshmallow.png');
+    this.load.image('candy-jelly', 'img/candy-jelly.png');
+    this.load.image('candy-donut', 'img/candy-donut.png');
+    this.load.image('candy-cupcake', 'img/candy-cupcake.png');
+    this.load.image('candy-pink', 'img/candy-pink.png');
+    this.load.image('candy-lollipop', 'img/candy-lollipop.png');
+    this.load.image('candy-icecream', 'img/candy-icecream.png');
+    this.load.image('candy-teddy', 'img/candy-teddy.png');
+    this.load.image('candy-cake', 'img/candy-cake.png');
+    this.load.image('candy-chocolate', 'img/candy-chocolate.png');
+    this.load.image('candy-super', 'img/candy-super.png');
+    this.load.image('candy-bomb', 'img/candy-bomb.png');
+    this.load.image('achievement-marshmallow', 'img/achievement-marshmallow.png');
+    this.load.image('achievement-jelly', 'img/achievement-jelly.png');
+    this.load.image('achievement-donut', 'img/achievement-donut.png');
+    this.load.image('achievement-cupcake', 'img/achievement-cupcake.png');
+    this.load.image('achievement-pink', 'img/achievement-pink.png');
+    this.load.image('achievement-lollipop', 'img/achievement-lollipop.png');
+    this.load.image('achievement-icecream', 'img/achievement-icecream.png');
+    this.load.image('achievement-teddy', 'img/achievement-teddy.png');
+    this.load.image('achievement-cake', 'img/achievement-cake.png');
+    this.load.image('achievement-chocolate', 'img/achievement-chocolate.png');
+    this.load.image('achievement-super', 'img/achievement-super.png');
+    this.load.image('achievement-cape', 'img/achievement-cape.png');
+    this.load.image('achievement-crown', 'img/achievement-crown.png');
+    this.load.image('achievement-win', 'img/achievement-win.png');
+    this.load.image('life-full', 'img/life-full.png');
+    this.load.image('life-empty', 'img/life-empty.png');
+    this.load.text('font-ttf', 'fonts/comicbook.ttf');
+    this.load.text('font-svg', 'fonts/comicbook.svg');
+    this.load.text('font-woff', 'fonts/comicbook.woff');
+    this.load.spritesheet('button-start', 'img/button-start.png', 401, 143);
+    this.load.spritesheet('button-alanswebsite', 'img/button-alans-website.png', 358, 133);
+    this.load.spritesheet('button-continue', 'img/button-continue.png', 358, 133);
+    this.load.spritesheet('button-back', 'img/button-back.png', 358, 133);
+    this.load.spritesheet('button-restart', 'img/button-restart.png', 363, 131);
+    this.load.spritesheet('button-achievements', 'img/button-achievements.png', 363, 131);
+    this.load.spritesheet('button-audio', 'img/button-audio.png', 111, 96);
+    this.load.spritesheet('button-pause', 'img/button-pause.png', 96, 98);
+    this.load.spritesheet('killmarker', 'img/killmarker.png', 83, 83);
+    this.load.spritesheet('monster-basic-idle', 'img/monster-basic-idle.png', 103, 131);
+    this.load.spritesheet('monster-basic-eats', 'img/monster-basic-eats.png', 111, 136);
+    this.load.spritesheet('monster-crown-idle', 'img/monster-crown-idle.png', 103, 144);
+    this.load.spritesheet('monster-crown-eats', 'img/monster-crown-eats.png', 127, 136);
+    this.load.spritesheet('monster-cape-idle', 'img/monster-cape-idle.png', 148, 127);
+    this.load.spritesheet('monster-cape-eats', 'img/monster-cape-eats.png', 148, 136);
+    this.load.spritesheet('monster-king-idle', 'img/monster-king-idle.png', 148, 144);
+    this.load.spritesheet('monster-king-eats', 'img/monster-king-eats.png', 148, 136);
+    this.load.audio('audio-click', ['audio/click.ogg', 'audio/click.mp3']);
+    this.load.audio('audio-eating', ['audio/eating.ogg', 'audio/eating.mp3']);
+    this.load.audio('audio-music', ['audio/music.ogg', 'audio/music.mp3']);
+    this.load.audio('audio-newlevel', ['audio/newlevel.ogg', 'audio/newlevel.mp3'])
+  },
+  create: function() {
+    this.state.start('MainMenu')
+  }
 };
